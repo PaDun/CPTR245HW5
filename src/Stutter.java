2fb2a9cebf831c0901150e71523eec0687ff5e87
phil0s0pher <c.lambert1213@gmail.com>
2017-10-31 14:36:01 -0700

    Refactored Code

--- src/Stutter.java
+++ src/Stutter.java
@@ -6,7 +6,7 @@ public class Stutter {
 
 
     public static String stut(String filePath) {
-        if(!filePath.substring(filePath.length()-4,filePath.length()).equals(".txt")) {
+        if(!isTextFile(filePath)) {
             return String.format("%s is not a txt file", filePath);
         }
 
@@ -18,30 +18,21 @@ public class Stutter {
             String output = "";
             String[] lineArray;
             int linecnt = 0;
-            String prevW;
 
             while ((line = br.readLine()) != null) {
                 ++linecnt;
-                lineArray = line.split("\\W+");
+                lineArray = getWords(line);
 
-                if (lineArray.length != 0) {
-                    prevW = lineArray[0];
-                    for (int i = 1; i < lineArray.length; i++) {
-                        if (lineArray[i].equals(prevW)) {
-                            output = output + String.format("%s ln %d, ", lineArray[i], linecnt);
-                            prevW = lineArray[i];
-                        } else {
-                            prevW = lineArray[i];
-                        }
-                    }
+                if (noWords(lineArray)) {
+                    output = getDuplicates(output, lineArray, linecnt);
                 }
             }
 
-            if(linecnt == 0 || output.length() == 0){
-                return "No Duplicates";
+            if(outputEmpty(output)) {
+                output = "No Duplicates";
             }
             else {
-                output = output.substring(0, output.length() - 2);
+                output = cleanUpOutput(output);
             }
 
             return output;
@@ -52,4 +43,41 @@ public class Stutter {
             return String.format("Error reading file: %s", filePath);
         }
     }
- }
+
+    private static String cleanUpOutput(String output) {
+        output = output.substring(0, output.length() - 2);
+        return output;
+    }
+
+    private static boolean noWords(String[] lineArray) {
+        return lineArray.length != 0;
+    }
+
+    private static boolean isTextFile(String filePath) {
+        return filePath.substring(filePath.length()-4,filePath.length()).equals(".txt");
+    }
+
+    private static boolean outputEmpty(String output) {
+        return output.length() == 0;
+    }
+
+    private static String[] getWords(String line) {
+        String[] lineArray;
+        lineArray = line.split("\\W+");
+        return lineArray;
+    }
+
+    private static String getDuplicates(String output, String[] lineArray, int linecnt) {
+        String prevW = lineArray[0];
+
+        for (int i = 1; i < lineArray.length; i++) {
+            if (lineArray[i].equals(prevW)) {
+                output = output + String.format("%s ln %d, ", lineArray[i], linecnt);
+                prevW = lineArray[i];
+            } else {
+                prevW = lineArray[i];
+            }
+        }
+        return output;
+    }
+}
