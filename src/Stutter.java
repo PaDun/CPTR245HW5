dddc4ccfa9e73278f4b616bac25d5263a7f1c071
phil0s0pher <c.lambert1213@gmail.com>
2017-10-30 19:23:56 -0700

    Added tests for file and empty file exceptions

--- src/Stutter.java
+++ src/Stutter.java
@@ -6,32 +6,41 @@ public class Stutter {
 
 
     public static String stut(String filePath) throws IOException {
-        FileReader fr = new FileReader(filePath);
-        BufferedReader br = new BufferedReader(fr);
-        String line;
-        String output = "";
-        String [] lineArray;
-        int linecnt = 0;
-        String prevW;
+        try {
+            FileReader fr = new FileReader(filePath);
+            BufferedReader br = new BufferedReader(fr);
 
-        while ((line = br.readLine()) != null) {
-            ++linecnt;
-            lineArray = line.split("\\W+");
+            String line;
+            String output = "";
+            String[] lineArray;
+            int linecnt = 0;
+            String prevW;
 
-            prevW =lineArray[0];
-            for (int i = 1; i < lineArray.length; i++) {
-                if(lineArray[i].equals(prevW)) {
-                    output = output + String.format("%s ln %d, ", lineArray[i], linecnt);
-                }
-                else {
-                    prevW = lineArray[i];
+            while ((line = br.readLine()) != null) {
+                ++linecnt;
+                lineArray = line.split("\\W+");
+
+                prevW = lineArray[0];
+                for (int i = 1; i < lineArray.length; i++) {
+                    if (lineArray[i].equals(prevW)) {
+                        output = output + String.format("%s ln %d, ", lineArray[i], linecnt);
+                    } else {
+                        prevW = lineArray[i];
+                    }
                 }
             }
+
+            if(output.length() == 0){
+                return "No Duplicates";
+            }
+            else {
+                output = output.substring(0, output.length() - 2);
+            }
+
+            return output;
+
+        } catch (FileNotFoundException ex) {
+            return String.format("File %s not found", filePath);
         }
-
-
-        return output;
-        //return "nibh ln 7, nostra ln 11";
     }
-
  }
--- tests/TestStutter.java
+++ tests/TestStutter.java
@@ -8,13 +8,23 @@ public class TestStutter {
 
     @Test
     public void testNormalBehavior() throws IOException {
-        assertEquals("Not expected return","nibh ln 7, nostra ln 11, ",
+        assertEquals("Not expected return","nibh ln 7, nostra ln 11",
                 Stutter.stut("/home/charlie/School/Fall_2017/Software_Testing/HW/CPTR245HW5/tests/blah.txt"));
     }
 
     @Test
     public void testNewBehavior() throws IOException {
-        assertEquals("Not expected return", "amet ln 1, tortor ln 2, metus ln 2, ",
+        assertEquals("Not expected return", "amet ln 1, tortor ln 2, metus ln 2",
                 Stutter.stut("/home/charlie/School/Fall_2017/Software_Testing/HW/CPTR245HW5/tests/blah2.txt"));
+    }
+
+    @Test
+    public void fileDoesNotExist() throws IOException {
+        assertEquals("not expected return", "File junk.txt not found", Stutter.stut("junk.txt"));
+    }
+
+    @Test
+    public void NoDupes() throws IOException {
+        assertEquals("not expected return", "No Duplicates", Stutter.stut("/home/charlie/School/Fall_2017/Software_Testing/HW/CPTR245HW5/tests/blah3.txt"));
     }
 }
